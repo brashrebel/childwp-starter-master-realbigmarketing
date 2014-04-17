1742813569896dd0f131f71bb68ca1171998bba9
Kyle <kyle@realbigmarketing.com>
2014-04-16 21:44:36 -0400

    First commit
    Real Big fun

---
+++ big.php
@@ -0,0 +1,18 @@
+<ul id="big-menu">
+	<li id="bi-link">
+		<a href="#">Big Ideas</a>
+		<div id="big-ideas" class="row real-big-nav">
+			<div class="small-2 large-4 column">Cool stuff here</div>
+			<div class="small-4 large-4 column">Cooler stuff here</div>
+			<div class="small-6 large-4 column">Coolest stuff here</div>
+		</div>
+	</li>
+	<li id="rr-link">
+		<a href="#">Real Results</a>
+		<div id="real-results" class="row real-big-nav">
+			<div class="small-2 large-4 column">Cool stuff here</div>
+			<div class="small-4 large-4 column">Cooler stuff here</div>
+			<div class="small-6 large-4 column">Coolest stuff here</div>
+		</div>
+	</li>
+</ul>
