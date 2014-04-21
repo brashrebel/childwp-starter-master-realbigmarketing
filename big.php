38c925af3cf0c1b0533a9e54550fab3d9642ea21
Kyle <kyle@realbigmarketing.com>
2014-04-21 15:05:42 -0400

    Header fixes and pagination style
    Now the big menu wraps, the text disappears next to the big menu icons
    on small screens and the pagination buttons color correctly.

--- big.php
+++ big.php
@@ -1,29 +1,29 @@
 <div id="big-menu"> 
 	<ul>
 		<li id="rr-link" class="top-link">
-			<a href="#"><span class="fa fa-gear"></span>Real Results</a>
+			<a href="#"><span class="fa fa-gear"></span></a>
 			<div id="real-results" class="row real-big-nav">
-				<div class="small-2 large-4 column">
+				<div class="large-4 column">
 					<?php if (dynamic_sidebar('real_results_1')) : else : endif; ?>
 				</div>
-				<div class="small-4 large-4 column">
+				<div class="large-4 column">
 					<?php if (dynamic_sidebar('real_results_2')) : else : endif; ?>
 				</div>
-				<div class="small-6 large-4 column">
+				<div class="large-4 column">
 					<?php if (dynamic_sidebar('real_results_3')) : else : endif; ?>
 				</div>
 			</div>
 		</li>
 		<li id="bi-link" class="top-link">
-			<a href="#"><span class="fa fa-bulb"></span>Big Ideas</a>
+			<a href="#"><span class="fa fa-bulb"></span></a>
 			<div id="big-ideas" class="row real-big-nav">
-				<div class="small-2 large-4 column">
+				<div class="large-4 column">
 					<?php if (dynamic_sidebar('big_ideas_1')) : else : endif; ?>
 				</div>
-				<div class="small-4 large-4 column">
+				<div class="large-4 column">
 					<?php if (dynamic_sidebar('big_ideas_2')) : else : endif; ?>
 				</div>
-				<div class="small-6 large-4 column">
+				<div class="large-4 column">
 					<?php if (dynamic_sidebar('big_ideas_3')) : else : endif; ?>
 				</div>
 			</div>
